5fdf7741c16cc7f9f16799a06040ace9736f9769
Sergeypervak <har4enko1983@gmail.com>
2022-07-24 01:20:57 +0300

    shop first preparing

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+    <footer className="page-footer #546e7a blue-grey darken-1">
+    <div className="footer-copyright">
+      <div className="container">
+      © {new Date().getFullYear()} All rights reserved by BUBUK
+      <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+      </div>
+    </div>
+    </footer>
+    )
+    }
+    
+    
+    
+    
+    export {Footer}
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return  <nav className="#37474f blue-grey darken-3">
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">React shop</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="!#">Sass</a></li>
+       
+      </ul>
+    </div>
+    </nav>
+        
+    
+    }
+    
+    export {Header};
