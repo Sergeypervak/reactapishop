6597661d0e579a1533046637de2356b2c05b3292
Sergeypervak <har4enko1983@gmail.com>
2022-08-06 21:42:00 +0300

    without issues

--- src/components/GoodsItem.jsx
+++ src/components/GoodsItem.jsx
@@ -4,9 +4,9 @@ function GoodsItem(props) {
         <div className="card" id={id}>
             <div className="card-image">
                 <img src={full_background} alt={name}/>
-                <span className="card-title">{name}</span>
             </div>
             <div className="card-content">
+            <span className="card-title">{name}</span>
                 <p>
                     {description}
                 </p>
