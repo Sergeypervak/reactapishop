0b508fca90ed02873606b2790e07ed89632313cd
Sergeypervak <har4enko1983@gmail.com>
2022-08-06 19:49:46 +0300

    hooks(state, effect

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,22 @@
+import {useState, useEffect} from "react";
+import {API_KEY,API_URL} from './config'
+
 function Shop() {
-    return <main className="container content"/>
+    const [goods, setGoods] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(function getGoods() {
+        fetch(API_URL, {
+            headers: {'Authorization': API_KEY,}
+        })
+            .then(response => response.json())
+            .then(data => {
+                  data.featured && setGoods(data.featured);
+                  setLoading(false);
+        });
+    }, []);
+
+    return <main className="container content">Shop</main>
 }
 
 
